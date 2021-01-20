d333f3f301f7f85a0bbdc76b760c9ea3c06e65ae
sjaugmented <sjaugmented@gmail.com>
2021-01-20 15:00:25 -0700

    added Hobby and Post types

--- schema/schema.js
+++ schema/schema.js
@@ -7,7 +7,23 @@ const usersData = [
 	{ id: '13', name: 'Anna', age: 26, profession: 'programmer' },
 	{ id: '211', name: 'Bella', age: 16, profession: 'dancer' },
 	{ id: '19', name: 'Gina', age: 26, profession: 'whore' },
-	{ id: '150', name: 'Goergina', age: 36, profession: 'puppetteer' },
+	{ id: '150', name: 'Georgina', age: 36, profession: 'puppetteer' },
+]
+
+const hobbiesData = [
+	{ id: '1', title: 'Photography', description: 'duh' },
+	{ id: '2', title: 'Programming', description: 'duh' },
+	{ id: '3', title: 'Writing', description: 'duh' },
+	{ id: '4', title: 'Fisting', description: 'duh' },
+	{ id: '5', title: 'Gaming', description: 'duh' },
+]
+
+const postsData = [
+	{ id: '1', comment: 'This is the first comment.' },
+	{ id: '2', comment: 'This is the second comment.' },
+	{ id: '3', comment: 'This is the third comment.' },
+	{ id: '4', comment: 'This is the fourth comment.' },
+	{ id: '5', comment: 'This is the last comment.' },
 ]
 
 const {
@@ -23,10 +39,29 @@ const UserType = new GraphQLObjectType({
 	name: 'User',
 	description: 'Documentation for user',
 	fields: () => ({
-		id: { type: GraphQLString },
+		id: { type: GraphQLID },
 		name: { type: GraphQLString },
 		age: { type: GraphQLInt },
 		profession: { type: GraphQLString },
+	}),
+})
+
+const HobbyType = new GraphQLObjectType({
+	name: 'Hobby',
+	description: 'Documentation for hobby',
+	fields: () => ({
+		id: { type: GraphQLID },
+		title: { type: GraphQLString },
+		description: { type: GraphQLString },
+	}),
+})
+
+const PostType = new GraphQLObjectType({
+	name: 'Post',
+	description: 'Documentation for post',
+	fields: () => ({
+		id: { type: GraphQLID },
+		comment: { type: GraphQLString },
 	}),
 })
 
@@ -37,11 +72,27 @@ const RootQuery = new GraphQLObjectType({
 	fields: {
 		user: {
 			type: UserType,
-			args: { id: { type: GraphQLString } },
+			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
 				// resolve with data
 				// get and return data from data source
 				return _.find(usersData, { id: args.id })
+			},
+		},
+
+		hobby: {
+			type: HobbyType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return _.find(hobbiesData, { id: args.id })
+			},
+		},
+
+		post: {
+			type: PostType,
+			args: { id: { type: GraphQLID } },
+			resolve(parent, args) {
+				return _.find(postsData, { id: args.id })
 			},
 		},
 	},
