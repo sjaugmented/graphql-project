f47f95eaf4d3978939ca0b14964876e4f15dba90
sjaugmented <sjaugmented@gmail.com>
2021-01-20 14:25:20 -0700

    depped lodash

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,14 @@
 const graphql = require("graphql")
 
+// dummy data
+const usersData = [
+  { id: "1", name: "Bond", age: 36 },
+  { id: "13", name: "Anna", age: 26 },
+  { id: "211", name: "Bella", age: 16 },
+  { id: "19", name: "Gina", age: 26 },
+  { id: "150", name: "Goergina", age: 36 },
+]
+
 const {
   GraphQLObjectType,
   GraphQLID,
@@ -30,6 +39,13 @@ const RootQuery = new GraphQLObjectType({
       resolve(parent, args) {
         // resolve with data
         // get and return data from data source
+        let user = {
+          id: "345",
+          name: "Paulo",
+          age: 33,
+        }
+
+        return user
       },
     },
   },
