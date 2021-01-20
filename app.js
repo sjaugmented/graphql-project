b085587d74ace7c72774945ce70eab05d8de88bc
sjaugmented <sjaugmented@gmail.com>
2021-01-20 14:43:30 -0700

    added profession field

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const { graphqlHTTP } = require("express-graphql")
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
 
-const schema = require("./schema/schema")
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.use(
-  "/graphql",
-  graphqlHTTP({
-    graphiql: true,
-    schema,
-  })
+	'/graphql',
+	graphqlHTTP({
+		graphiql: true,
+		schema,
+	})
 )
 
 app.listen(4000, () => {
-  console.log("listening on port 4000")
+	console.log('listening on port 4000')
 })
--- schema/schema.js
+++ schema/schema.js
@@ -1,56 +1,52 @@
-const graphql = require("graphql")
+const graphql = require('graphql')
+const _ = require('lodash')
 
 // dummy data
 const usersData = [
-  { id: "1", name: "Bond", age: 36 },
-  { id: "13", name: "Anna", age: 26 },
-  { id: "211", name: "Bella", age: 16 },
-  { id: "19", name: "Gina", age: 26 },
-  { id: "150", name: "Goergina", age: 36 },
+	{ id: '1', name: 'Bond', age: 36, profession: 'director' },
+	{ id: '13', name: 'Anna', age: 26, profession: 'programmer' },
+	{ id: '211', name: 'Bella', age: 16, profession: 'dancer' },
+	{ id: '19', name: 'Gina', age: 26, profession: 'whore' },
+	{ id: '150', name: 'Goergina', age: 36, profession: 'puppetteer' },
 ]
 
 const {
-  GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLInt,
-  GraphQLSchema,
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
 } = graphql
 
 // Create types
 const UserType = new GraphQLObjectType({
-  name: "User",
-  description: "Documentation for user",
-  fields: () => ({
-    id: { type: GraphQLString },
-    name: { type: GraphQLString },
-    age: { type: GraphQLInt },
-  }),
+	name: 'User',
+	description: 'Documentation for user',
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		age: { type: GraphQLInt },
+		profession: { type: GraphQLString },
+	}),
 })
 
 // RootQuery
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
-  description: "Description",
-  fields: {
-    user: {
-      type: UserType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {
-        // resolve with data
-        // get and return data from data source
-        let user = {
-          id: "345",
-          name: "Paulo",
-          age: 33,
-        }
-
-        return user
-      },
-    },
-  },
+	name: 'RootQueryType',
+	description: 'Description',
+	fields: {
+		user: {
+			type: UserType,
+			args: { id: { type: GraphQLString } },
+			resolve(parent, args) {
+				// resolve with data
+				// get and return data from data source
+				return _.find(usersData, { id: args.id })
+			},
+		},
+	},
 })
 
 module.exports = new GraphQLSchema({
-  query: RootQuery,
+	query: RootQuery,
 })
